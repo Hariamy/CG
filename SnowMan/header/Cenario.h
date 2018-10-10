baeacbe2ce0cc46e89630cb091aaf6a3c1df6b79
hariamy <hariamymv@hotmail.com>
2018-10-10 08:20:37 -0300

    Com camera, cone, e varias luzes

--- SnowMan/header/Cenario.h
+++ SnowMan/header/Cenario.h
@@ -5,4 +5,4 @@
 #include "Cone.h"
 
 void setCenario(Objeto *cenario[10], Luz *luzes[10], int *quantObjetos);
-void corPint(Objeto *obj, double Pint[3], Luz *luz, double Iamb[3], double *Ipix, double quant);
+void corPint(Objeto *obj, double Pint[3], Luz *luzes[10], double Iamb[3], double *Ipix, double quant, int quantLuzes);
