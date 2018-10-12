fb372e8f8b2c245d1a466601acd62d62f046905f
hariamy <hariamymv@hotmail.com>
2018-10-12 07:17:43 -0300

    Coisos

--- SnowMan/header/Cenario.h
+++ SnowMan/header/Cenario.h
@@ -4,5 +4,8 @@
 #include "Esfera.h"
 #include "Cone.h"
 
-void setCenario(Objeto *cenario[10], Luz *luzes[10], int *quantObjetos);
-void corPint(Objeto *obj, double Pint[3], Luz *luzes[10], double Iamb[3], double *Ipix, double quant, int quantLuzes);
+void setCenario(Objeto *cenario[10], Luz *luzes[10], int *quantObjetos, int *quantLuzes);
+//void corPint(Objeto *obj, double Pint[3], Luz *luzes[10], double Iamb[3], double *Ipix, bool sombra, int quantLuzes);
+void corLuz(Objeto *obj, double Pint[3], Luz *luz, double *Ipix);
+void corAmbriente(Objeto *obj, double Iamb[3], double *Ipix);
+
